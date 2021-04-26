1a9f4ca4cc5813af31217f48996c66c9b0704a63
Robson Cruz <ghost0092@gmail.com>
2021-04-26 20:05:33 -0300

    test: add API tests to searching endpoint

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -12,7 +15,7 @@ func TestFormattingListWithTwoComponents(t *testing.T) {
 		{"teste teste", "12345"},
 	}
 
-	result := format_company_data(initial_list)
+	result := formatCompanyData(initial_list)
 	expected_result := [][]string{
 		{"Teste Teste", "12345"},
 	}
@@ -28,7 +31,7 @@ func TestFormattingListWithThreeComponents(t *testing.T) {
 		{"teste teste", "12345", "http://WWW.SIte.com"},
 	}
 
-	result := format_company_data(initial_list)
+	result := formatCompanyData(initial_list)
 	expected_result := [][]string{
 		{"Teste Teste", "12345", "http://www.site.com"},
 	}
@@ -36,5 +39,55 @@ func TestFormattingListWithThreeComponents(t *testing.T) {
 	if strings.Compare(result[0][0], expected_result[0][0]) != 0 || strings.Compare(result[0][1], expected_result[0][1]) != 0 || strings.Compare(result[0][2], expected_result[0][2]) != 0 {
 		t.Log("Error, the result was ", result, " while the expectation was ", expected_result)
 		t.Fail()
+	}
+}
+
+func TestSearchingWithoutBodyGivesError(t *testing.T) {
+	req, err := http.NewRequest("GET", "/companies/search", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(searchCompany)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("HTTP handler returned wrong status code: got %v but expected %v", status, http.StatusBadRequest)
+	}
+}
+
+func TestSearchingReturns404IfNoResults(t *testing.T) {
+	jsonBody := []byte(`{"name":"ZZZZZZ", "zip_code": "99999"}`)
+	req, err := http.NewRequest("GET", "/companies/search", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(searchCompany)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("HTTP handler returned wrong status code: got %v but expected %v", status, http.StatusBadRequest)
+	}
+}
+
+func TestSearchingReturnsDesiredCompanyIfFoundWithStatus200(t *testing.T) {
+	comp := Company{Company_name: "Test Company", Zip_Code: "99999", Id: 999}
+	db := createConnection()
+	db.Create(&comp)
+	jsonBody := []byte(`{"name":"Test", "zip_code": "99999"}`)
+	req, err := http.NewRequest("GET", "/companies/search", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(searchCompany)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("HTTP handler returned wrong status code: got %v but expected %v", status, http.StatusOK)
+	}
+
+	expected := `{"id":999,"name":"Test Company","zip":"99999","website":""}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
 	}
 }
